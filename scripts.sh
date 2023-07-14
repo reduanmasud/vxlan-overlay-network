686eba6153c18b19a0a681d96343b8196077cfea
Md. Reduan Masud <59122533+reduanmasud@users.noreply.github.com>
2023-07-14 19:31:28 +0600

    Create Subnet using docker network script added

--- scripts.sh
+++ scripts.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Set the color variable
+red='\033[0;31m'
 green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
 # Clear the color after that
 clear='\033[0m'
 
@@ -11,3 +16,10 @@ sudo apt update
 
 echo -e "${green} [2 ] ${clear} Installing Docker.io ... ... ..."
 sudo apt install -y docker.io
+
+
+echo -e "${green} [3 ] ${clear} Creating subnet using docker network ... ... ..."
+echo -e -n "${red} Enter IP address with CIDR (ip/cidr) : ${clear}"
+read -r docker_subnet_ip
+echo -n "Docker Subnet key: " >> save.txt
+sudo docker network create --subnet $docker_subnet_ip vxlan-net >> save.txt
